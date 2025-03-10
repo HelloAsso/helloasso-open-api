afb2477672ff2783c4bd969fc2eef66eb6f55471
emnbdx <eddy.montus@gmail.com>
2025-03-10 14:41:37 +0100

    add github action to auto generate sdk and push to readme

--- generate-sdk.sh
+++
@@ -1,3 +0,0 @@
-openapi-generator generate -i helloasso.json -g php -o ../helloasso-php
-openapi-generator generate -i helloasso.json -g javascript -o ../helloasso-node
-openapi-generator generate -i helloasso.json -g python -o ../helloasso-python --additional-properties=packageName=helloasso-python
---
+++ index.js
@@ -0,0 +1,29 @@
+import 'dotenv/config'
+import axios from "axios";
+
+async function uploadToReadMe(jsonString) {
+    const formData = new FormData();
+    formData.append('spec', jsonString);
+
+    const config = {
+        headers: {
+            'accept': 'application/json',
+            'x-readme-version': 'v1',
+            'Authorization': `Basic ${Buffer.from(`${process.env.README_API_KEY}:`).toString('base64')}`
+        }
+    };
+
+    try {
+        const response = await axios.put(`https://dash.readme.com/api/v1/api-specification/${process.env.API_ID}`, formData, config);
+        console.log('Swagger successfully uploaded:', response.data);
+    } catch (error) {
+        console.error('Failed to upload Swagger file:', error);
+    }
+}
+
+function pushToReadme() {
+    const jsonString = fs.readFileSync('helloasso.json', 'utf8');
+    uploadToReadMe(jsonString);
+}
+
+pushToReadme();
